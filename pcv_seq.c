a4dff094faa4a1b149b1f4d76e09a9851869ef2b
Carlos Henrique Lima Melara <charlesmelara@outlook.com>
2020-06-20 19:08:25 -0300

    Continuação

--- pcv_seq.c
+++ pcv_seq.c
@@ -3,11 +3,13 @@
 #include <omp.h>
 #include <mpi.h>
 
+#define ORIGEM 0
+
 int main(int argc, char ** argv) {
-	// Declara as matrizes
-	int *A,*B;
+	// Declara a matrize
+	int *A;
 	// Declara as variáveis de índice
-	int i,j,dim;
+	int i, j, dim, min;
 	// Declara o acumulador para o produto escalar global
 	long resultado=0;
 
@@ -18,15 +20,18 @@ int main(int argc, char ** argv) {
  
 	// Aloca as matrizes
 	A=(int *)malloc(dim *dim * sizeof(int));
-	B=(int *)malloc(dim *dim * sizeof(int)); 
 
 	// Aloca um vetor para armazenar os produtos escalares de cada linha
 	prod_escalar=(long *)malloc(dim * sizeof(long));
 
-	 // Lê a matriz A
+	// Lê a matriz A
 	for(i=0;i<dim;i++){
 		for(j=0;j<dim;j++){
 			fscanf(stdin, "%d ",&(A[i*dim+j]));
 		}
 	}
+
+	int *caminho = (int *) calloc(dim, sizeof(int));
+
+	min = find_min_path(ORIGEM, vertices, caminho);
 }
